56687a3693398c1f9eaf312f57547c62246d2294
CCphobia <peteros9999@gmail.com>
2020-03-20 21:34:08 +0100

    Initial commit

---
+++ src/test/java/com/project/quizbase/QuizBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quizbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
